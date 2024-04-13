ebad6ab6c4b6b399891b83c02352ec65df29b40c
Kristiyan Dzhakov <kristiyandzhakov@gmail.com>
2024-04-13 20:20:28 +0100

    chore: add plaveholder query to fetch ranked users

--- models/query_mapper/query_mapper.go
+++ models/query_mapper/query_mapper.go
@@ -29,3 +29,30 @@ func (dao *UserDAO) FetchLoggedUserLatLong(loggedInUserID string) (*sql.Rows, er
 	defer stmt.Close()
 	return stmt.Query(loggedInUserID)
 }
+
+func (dao *UserDAO) FetchUsersWithRank(loggedInUserID string) (*sql.Rows, error) {
+	stmt, err := dao.db.Prepare(
+		`SELECT
+			u.id AS user_id,
+			u.email,
+			u.password_hash,
+			u.name,
+			u.gender,
+			u.age,
+			u.latitude,
+			u.longitude,
+			u.created_at,
+			u.updated_at,
+			IFNULL(COUNT(i.swipe_direction), 0) AS yes_swipes
+		FROM users u
+		LEFT JOIN interactions i ON u.id = i.swiped_id AND i.swipe_direction = 'YES'
+		WHERE u.id != ?
+		GROUP BY u.id, u.email, u.password_hash, u.name, u.gender, u.age, u.latitude, u.longitude, u.created_at, u.updated_at
+		ORDER BY yes_swipes DESC;`)
+
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	return stmt.Query(loggedInUserID)
+}
